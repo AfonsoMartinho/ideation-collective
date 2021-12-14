a7f32117dd2552e1246ecd65e5171fe03becdcd2
Afonso Martinho <afonso.martinho@namecheap.com>
2021-12-14 15:37:59 +0000

    added eslint

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import Button from "../components/button";
 import Tag from "../components/tag";
