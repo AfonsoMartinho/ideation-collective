d29affea069fc05892dc327b28227f6b567a753e
Afonso Martinho <afonso.martinho@namecheap.com>
2021-12-14 16:09:51 +0000

    ignorin eslint on build

--- next.config.js
+++ next.config.js
@@ -9,5 +9,10 @@ module.exports = {
     /* Add Your Scss File Folder Path Here */
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
-    }
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
 }
