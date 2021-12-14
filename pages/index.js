ac59f4d0bdc5fbd3399c47d6cb40b62be6137f27
Afonso Martinho <afonso.martinho@namecheap.com>
2021-12-14 16:00:00 +0000

    linted Project

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import Button from "../components/button";
-import Tag from "../components/tag";
 import HorizontalCarousel from "../components/HorizontalCarousel";
 import TestimonialCard from "../components/testimonialCard";
 
